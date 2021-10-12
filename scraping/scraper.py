e7d64558a3cdf3f96294e76f44c5d29bf4cfa8fd
Tyler Jang <taj26@duke.edu>
2021-10-12 02:08:32 -0400

    added scraping skeleton

---
+++ scraping/scraper.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 01:50:35 2021
+
+@author: Pikachooze
+"""
+import requests
+
+# Database reference, only missing the Move-Pokemon connecting table (assume just a (poke_id, move_id)) relation.
+# https://dbdiagram.io/d/6158de67825b5b01461dfd3f
+
+# %% Constants
+POKEAPI_HOSTNAME = "https://pokeapi.co/api/v2/"
+SPECIES_LIST_EXT = "pokemon-species"
+POKEMON_EXT = "pokemon/{:}"
+MOVES = "moves"
+
+# %% Global Variables
+move_list = [] #could be a map, or use enumerate when you need to, in order to preserve foreign keys
+
+# %% Helper Methods
+def get_limit_and_offset(limit, offset):
+    return "?limit={:d}&offset={:d}".format(limit, offset)
+
+def request_to_api(extension):
+    response = requests.get(POKEAPI_HOSTNAME + extension)
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("ERROR: STATUS CODE: {:}".format(response.status_code))
+
+def get_piplup():
+    res = request_to_api(POKEMON_EXT.format("piplup"))
+    moves = res[MOVES]
+    for move in moves:
+        print(move["move"]["name"])
+    #print(res)
+
+# %% Scraping Methods
+
+
+
+# %% Main
+if __name__ == "__main__":
+    print("pika!")
+    get_piplup()
