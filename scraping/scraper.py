9fb1e9f86beac1dd00c294ecbc7976ba29d13c1f
Tyler Jang <taj26@duke.edu>
2021-10-12 02:08:32 -0400

    added scraping skeleton

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -6,7 +6,6 @@ Created on Tue Oct 12 01:50:35 2021
 @author: Pikachooze
 """
 import requests
-import pandas as pd
 
 # Database reference, only missing the Move-Pokemon connecting table (assume just a (poke_id, move_id)) relation.
 # https://dbdiagram.io/d/6158de67825b5b01461dfd3f
@@ -16,8 +15,6 @@ POKEAPI_HOSTNAME = "https://pokeapi.co/api/v2/"
 SPECIES_LIST_EXT = "pokemon-species"
 POKEMON_EXT = "pokemon/{:}"
 MOVES = "moves"
-TYPES = "type"
-type_dict = {}
 # %% Global Variables
 move_list = [] #could be a map, or use enumerate when you need to, in order to preserve foreign keys
 
@@ -39,27 +36,11 @@ def get_piplup():
         print(move["move"]["name"])
     #print(res)
 
-def get_types():
-
-    count = 1
-    typelist = []
-    res = request_to_api(TYPES)
-    for type in res["results"][:-2]:
-        type_dict[type["name"]] = count
-        typelist.append([count, type["name"]])
-        count += 1
-    print(type_dict)
-    print(typelist)
-
-    df_type = pd.DataFrame(typelist)
-    df_type = df_type.to_csv('db/data/Types.csv', index = False, header = False)
-    print(df_type)
-    
-
 # %% Scraping Methods
 
 
 
 # %% Main
 if __name__ == "__main__":
-    get_types()
+    print("pika!")
+    get_piplup()
