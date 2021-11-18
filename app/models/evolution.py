59eb889d29444d6c04d59042c467b8328bea6bfa
Michelle Mao <mm738@duke.edu>
2021-11-17 23:18:40 -0500

    evolutions

---
+++ app/models/evolution.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from app.models.base import Base
+
+
+
+class Evolution(Base):
+    __tablename__ = 'evolution'
+
+    poke1_id = Column(Integer, ForeignKey('pokemon.poke_id'), primary_key =True, nullabe = False)
+    poke2_id = Column(Integer, ForeignKey('pokemon.poke_id'), primary_key = True, nullabe = False)
+  
+    poke1 = relationship("Pokemon", foreign_keys=[poke1_id]) 
+    poke2 = relationship("Pokemon", foreign_keys=[poke2_id])
+
+    def __repr__(self):
+        return "<Evolution(poke1_id='%d', poke2_id='%d')>" % (
+                             self.poke1_id, self.poke2_id)
